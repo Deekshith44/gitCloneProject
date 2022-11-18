dd597b4fe1b7437ef21f1e2fb8c83dc686c67a22
Deekshith <99590643+Deekshith44@users.noreply.github.com>
2022-11-18 15:25:33 +0530

    Git clone website

---
+++ GitClone/app.js
@@ -0,0 +1,80 @@
+const APIURL="https://api.github.com/users/";
+const main=document.querySelector('#main');
+const searchBox=document.querySelector('#search');
+const getUser=async (username)=>
+{
+    const response=await fetch(APIURL+username);
+    const data=await response.json();   //getting the data from API ,after converting to Json and storing in data
+    
+    const card=`
+    <div class="card">
+            <div>
+                <img src="${data.avatar_url}" class="avatar">
+            </div>
+
+            <div class="basic-info">
+                <h2>${data.name}</h2>
+                <p>${data.bio}</p>
+            
+            
+            <ul class="info">
+                <li>${data.followers}<strong>Followers</strong></li>
+                <li>${data.following}<strong>Following</strong></li>
+                <li>${data.public_repos}<strong>Repos</strong></li>
+            </ul>
+            
+            <div class="repos">
+              
+            </div>
+            </div>
+</div>
+                
+    `;
+
+    main.innerHTML=card;          //newt type
+    getRepos(username);
+}
+
+getUser('Deekshith44');
+
+
+const getRepos= async (username)=>
+{
+    const repos=document.querySelector('.repos');
+    const response=await fetch(APIURL+username +"/repos");
+    const data=await response.json();
+    
+    data.forEach((item)=>
+    {
+        const elem=document.createElement('a');
+        elem.classList.add('repo');
+        elem.href=item.html_url;  //if u gave only url as in the console u get wrong type so ude html_
+        elem.innerText=item.name;
+        elem.target="_blank";
+        repos.appendChild(elem);
+    })
+}
+
+
+const formSubmit=()=>
+{
+
+if(searchBox.value!="")
+{
+    getUser(searchBox.value);
+    searchBox.value="";
+}
+return false;
+
+}
+
+searchBox.addEventListener('focusout',function()
+{
+    formSubmit();
+})
+
+/*
+  <a class="repo" href="#" target="_blank">Repo 1</a>
+                <a class="repo" href="#" target="_blank">Repo 1</a>
+                <a class="repo" href="#" target="_blank">Repo 1</a>
+*/
